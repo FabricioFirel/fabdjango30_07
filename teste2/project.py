21a6c046d55efee5897d1cb8885d4a0a27a2a7c7
Fabrício Alves <161898642+FabricioFirel@users.noreply.github.com>
2026-08-06 21:00:26 -0300

    Update project.py

--- teste2/project.py
+++ teste2/project.py
@@ -1,6 +1,38 @@
+def adicionar():
+    print("\n=== Adicionar Item ===\n")
+    chave = input("Digite o nome do item: ")
+    valor = int(input("Digite a quantidade do item: "))
+
+    if chave in estoque:
+        print("Item já existe no estoque.")
+        return
+    else:
+        estoque[chave] = valor
+        return
+
 def main():
-    estoque={"caneta azul":10, "caderno":10, "lapis":40}
+    estoque={"Caneta": 10, "Caderno": 5, "Lápis": 20}
 
-    print("\n==Menu==")
+    while True:
+        print("Menu:")
+        print("1. Adicionar item")
+        print("2. Remover item")
+        print("3. Listar")
+        print("4. Sair")
+        
 
-main()
+        op = input("Escolha uma opção: ")
+
+
+
+        match op:
+            case "1":
+                adicionar(estoque)
+             case "2":
+                print("Option 2 selected")
+            case "3":
+                print("Option 3 selected")    
+            case "4":
+                print("Option 4 selected")          
+            case _:
+                print("Invalid option selected")
